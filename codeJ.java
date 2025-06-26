867ee36dc0f95b4228aa99af1f96c6b9cd0a9304
Keerthana V <keertanv1975@gmail.com>
2025-06-26 17:23:09 +0530

    My java inhe

---
+++ codeJ.java
@@ -0,0 +1,13 @@
+class A{
+  int a,b; 
+
+}
+class B extends class A{
+  a=23
+    System.out.println("A" + a);
+  }
+  public class demo{
+     public static void main(String[] args){
+      B b = new B();
+     }
+  }
